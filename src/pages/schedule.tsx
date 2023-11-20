e09c8b7a209c85124f47d19dc87c0ae93ba1c8c1
Edward Hockedy <edhockedy@hotmail.com>
2023-11-20 22:40:26 +0000

    Started work on schedule page content

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -2,8 +2,31 @@
 import { jsx } from "theme-ui";
 import type { HeadFC } from "gatsby";
 
+type EventProps = {
+  name: string;
+  time: string;
+}
+
+const Event = ({ name, time }: EventProps) => <div sx={{
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  my: 3,
+}}>
+  <div sx={{ fontWeight: 'bold' }} >{time}</div>
+  <div>{name}</div>
+</div>
+
 const SchedulePage = () => {
-  return <div>Schedule</div>;
+  return <div sx={{
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  }}>
+    <Event name="Arrival at church" time="13:00" />
+    <Event name="Ceremony begins" time="13:30" />
+
+  </div>;
 };
 
 export default SchedulePage;
