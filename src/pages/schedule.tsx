4fca331e3e19fbb6b41896518173f9075600a71f
Rosalind Keates <rosalind.keates@gmail.com>
2023-12-10 19:48:47 +0000

    added info to events in schedule

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -5,16 +5,19 @@ import type { HeadFC } from "gatsby";
 type EventProps = {
   name: string;
   time: string;
+  info?: string;
 }
 
-const Event = ({ name, time }: EventProps) => <div sx={{
+const Event = ({ name, time, info }: EventProps) => <div sx={{
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
   my: 3,
+  fontSize: 18,
 }}>
   <div sx={{ fontWeight: 'bold' }} >{time}</div>
   <div>{name}</div>
+  {info && <div sx={{ fontSize: 16, fontStyle: 'italic'  }}>{info}</div>}
 </div>
 
 const SchedulePage = () => {
@@ -23,9 +26,10 @@ const SchedulePage = () => {
     flexDirection: "column",
     alignItems: "center",
   }}>
-    <Event name="Arrival at St George's Church, Brinsop" time="13:00" />
+    <Event name="Guests can arrive at Brinsop Court" time="12:00" info="Guests staying onsite can check in at this time and leave their bags at reception to be brought to their rooms."/>
+	<Event name="Arrival at St George's Church, Brinsop" time="13:00" />
     <Event name="Ceremony begins" time="13:30" />
-	<Event name="Drinks Reception at Brinsop Court" time="15:00" />
+	<Event name="Drinks Reception at Brinsop Court" time="15:00" info="Drinks and nibbles will be served, and there will be a seleciton of lawn games on the, you guessed it, lawn."/>
 	<Event name="Meal Begins" time="17:30" />
 	<Event name="The Windy City Seven" time="19:00" />
 	<Event name="Butler Jazz Band" time="20:45" />
