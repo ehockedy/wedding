ca4eb6872ad555e1be7645d127af4f0ec7c222c0
Edward Hockedy <edhockedy@hotmail.com>
2024-03-05 23:13:37 +0000

    Responsive food form size

--- src/pages/foodChoices.tsx
+++ src/pages/foodChoices.tsx
@@ -6,10 +6,11 @@ const FoodChoicesPage = () => {
     return (
         <iframe
             src="https://docs.google.com/forms/d/e/1FAIpQLSeWaGjWg3RFOM9B6vs8-XHXvcCMZ6pmyOlpzsAbB3HHej0ijA/viewform?embedded=true"
-            height="465px"
+            height="100%"
             sx={{
                 border: "none",
                 width: ["94vw", "100%", "100%"],
+                height: ["465px", "465px", "500px"]
             }}
         >
             Loading…
